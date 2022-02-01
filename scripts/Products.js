914ca2399dc50479e4a36582785a9ae74cc39277
Hilary Shaner <hilaryshaner@gmail.com>
2022-02-01 09:15:40 -0600

    click events working, haven't done explorer ch

--- scripts/Products.js
+++ scripts/Products.js
@@ -1,5 +1,21 @@
 import { getProducts } from "./database.js"
 
+//this click event displays the price of each product
+document.addEventListener(
+    "click",
+    (clickEvent) => {
+        const itemClicked = clickEvent.target
+        if (itemClicked.id.startsWith("product")) {
+            const [,productId] = itemClicked.id.split("--")
+            for (const product of products) {
+                if (product.id === parseInt(productId)) {
+                    window.alert(`${product.name} costs $${product.price}.`)
+               }
+           }
+       }
+   }
+)
+
 //invokes the imported function and assigns it to a variable
 const products = getProducts()
 
@@ -10,9 +26,6 @@ export const Products = () => {
     for (const product of products) {
         html += `<li id="product--${product.id}">${product.name}</li>`
     }
-
     html += "</ul>"
-
     return html
 }
-
